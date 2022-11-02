3ca7b730c75a60cd7b1b924c03957b6d8dd58341
Nagae Hidetake <nagae@eagan.jp>
2022-11-03 05:04:38 +0900

    Byte -> UByte

--- android/app/src/main/java/jp/eagan/tlscertviewer/Base64.kt
+++ android/app/src/main/java/jp/eagan/tlscertviewer/Base64.kt
@@ -1,21 +1,21 @@
 package jp.eagan.tlscertviewer
 
 class Base64 {
-    class CodeRange(val charMin: Char, val charMax: Char, val codeMin: Byte, val codeMax: Byte)
+    class CodeRange(val charMin: Char, val charMax: Char, val codeMin: UByte, val codeMax: UByte)
 
     companion object {
         private val base64CodeTable = arrayOf(
-            CodeRange('A', 'Z', 0b000000, 0b011001),
-            CodeRange('a', 'z', 0b011010, 0b110011),
-            CodeRange('0', '9', 0b110100, 0b111101),
-            CodeRange('+', '+', 0b111110, 0b111110),
-            CodeRange('/', '/', 0b111111, 0b111111)
+            CodeRange('A', 'Z', 0b000000u, 0b011001u),
+            CodeRange('a', 'z', 0b011010u, 0b110011u),
+            CodeRange('0', '9', 0b110100u, 0b111101u),
+            CodeRange('+', '+', 0b111110u, 0b111110u),
+            CodeRange('/', '/', 0b111111u, 0b111111u)
         )
 
         private val decodeMask = arrayOf(
-            arrayOf(0b00111111, 0b00110000, 0b00000000, 0b00000000),
-            arrayOf(0b00000000, 0b00001111, 0b00111100, 0b00000000),
-            arrayOf(0b00000000, 0b00000000, 0b00000011, 0b00111111)
+            arrayOf(0b00111111u, 0b00110000u, 0b00000000u, 0b00000000u),
+            arrayOf(0b00000000u, 0b00001111u, 0b00111100u, 0b00000000u),
+            arrayOf(0b00000000u, 0b00000000u, 0b00000011u, 0b00111111u)
         )
         private val decodeShift = arrayOf(
             arrayOf(2, -4, 0, 0),
@@ -30,10 +30,10 @@ class Base64 {
          *  @return input に対応する 6 ビット数値
          *  @exception IllegalArgumentException input が Base64 で未定義の文字の場合
          */
-        fun c2b(input: Char): Byte {
+        fun c2b(input: Char): UByte {
             for (i in base64CodeTable) {
                 if (i.charMin <= input && input <= i.charMax) {
-                    return (i.codeMin + (input - i.charMin)).toByte()
+                    return (i.codeMin + (input - i.charMin).toUInt()).toUByte()
                 }
             }
             throw IllegalArgumentException("illegal character for base64: %c".format(input))
@@ -46,10 +46,10 @@ class Base64 {
          * @return input に対応する ASCII 文字
          * @exception IllegalArgumentException input が 6 ビット数値の範囲外だった場合
          */
-        fun b2c(input: Byte): Char {
+        fun b2c(input: UByte): Char {
             for (i in base64CodeTable) {
                 if (i.codeMin <= input && input <= i.codeMax) {
-                    return (i.charMin + (input - i.codeMin))
+                    return (i.charMin + (input - i.codeMin).toInt())
                 }
             }
             throw IllegalArgumentException("illegal code for base64: %d".format(input))
@@ -61,24 +61,24 @@ class Base64 {
          * @param input 6 ビット数値列（4 個）
          * @return 変換結果の 8 ビット数値列（3 個）
          */
-        fun pack64(input: Array<Byte>): Array<Byte> {
+        fun pack64(input: Array<UByte>): Array<UByte> {
             if (input.size != decodeMask[0].size){
                 throw IllegalArgumentException("illegal input length: %d".format(input.size))
             }
-            val inputInt = input.map { it.toInt() }
-            val output = arrayOf<Byte>(0, 0, 0)
+            val inputInt = input.map { it.toUInt() }
+            val output = arrayOf<UByte>(0u, 0u, 0u)
 
             for (i in 0 until output.size) {
-                var outputInt = 0
+                var outputInt = 0u
                 for (j in 0 until input.size) {
                     val masked = inputInt[j] and decodeMask[i][j]
                     val shifted = if (decodeShift[i][j] >= 0)
                                             masked shl decodeShift[i][j]
                                     else
-                                            masked ushr -decodeShift[i][j]
+                                            masked shr -decodeShift[i][j]
                     outputInt = outputInt or shifted
                 }
-                output[i] = outputInt.toByte()
+                output[i] = outputInt.toUByte()
             }
            return output
         }
@@ -90,9 +90,9 @@ class Base64 {
          * @param flags 未実装（無視される）
          * @return input に対応するバイト列
          */
-        fun decode(input: String, flags: Int): ByteArray {
-            val inputCode = arrayOf<Byte>(0, 0, 0, 0)
-            val output = mutableListOf<Byte>()
+        fun decode(input: String, flags: Int): UByteArray {
+            val inputCode = arrayOf<UByte>(0u, 0u, 0u, 0u)
+            val output = mutableListOf<UByte>()
 
             for ((index, c) in input.withIndex()) {
                 inputCode[index % inputCode.size] = c2b(c)
@@ -100,7 +100,7 @@ class Base64 {
                     pack64(inputCode).forEach { output.add(it) }
                 }
             }
-            return output.toByteArray()
+            return output.toUByteArray()
         }
 
     }
