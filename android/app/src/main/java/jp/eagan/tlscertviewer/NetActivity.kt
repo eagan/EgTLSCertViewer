2c62ed25691139225dea5286fe9023fc4ec476f8
Nagae Hidetake <nagae@eagan.jp>
2022-12-31 19:06:27 +0900

    activity Net

---
+++ android/app/src/main/java/jp/eagan/tlscertviewer/NetActivity.kt
@@ -0,0 +1,11 @@
+package jp.eagan.tlscertviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_net)
+    }
+}
