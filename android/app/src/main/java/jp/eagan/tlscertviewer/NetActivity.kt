8e5326f4b4a0bc93a77297b437c7bc3bdac51726
Nagae Hidetake <nagae@eagan.jp>
2022-12-31 20:56:34 +0900

    protocol spinner

--- android/app/src/main/java/jp/eagan/tlscertviewer/NetActivity.kt
+++ android/app/src/main/java/jp/eagan/tlscertviewer/NetActivity.kt
@@ -2,10 +2,36 @@ package jp.eagan.tlscertviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.Spinner
+import android.widget.SpinnerAdapter
 
 class NetActivity : AppCompatActivity() {
+    class Protocol(val label:String, val port: Int)
+
+    val PROTOCOL_LIST = arrayOf(
+        Protocol("HTTPS", 443),
+        Protocol("IMAPS", 993),
+        Protocol("IMAP (STARTTLS)", 143),
+        Protocol("SMTPS", 465),
+        Protocol("SMTP (STARTTLS)", 25),
+        Protocol("POP3S", 995)
+    )
+    val PROTOCOL_LABEL_LIST = PROTOCOL_LIST.map{ it.label }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_net)
+
+        val protocol: Spinner = findViewById(R.id.net_protocol)
+        val protocolAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            PROTOCOL_LABEL_LIST
+        ).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        protocol.adapter = protocolAdapter
     }
 }
