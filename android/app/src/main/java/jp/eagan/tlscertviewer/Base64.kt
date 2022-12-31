0b933da140f38fd481b47c44c8fdfc01ba898190
Nagae Hidetake <nagae@eagan.jp>
2022-12-31 13:57:08 +0900

    Refactor Bse64.decode

--- android/app/src/main/java/jp/eagan/tlscertviewer/Base64.kt
+++ android/app/src/main/java/jp/eagan/tlscertviewer/Base64.kt
@@ -13,14 +13,16 @@ class Base64 {
         )
 
         private val decodeMask = arrayOf(
-            arrayOf(0b00111111, 0b00110000, 0b00000000, 0b00000000),
-            arrayOf(0b00000000, 0b00001111, 0b00111100, 0b00000000),
-            arrayOf(0b00000000, 0b00000000, 0b00000011, 0b00111111)
+            arrayOf(0b00000000, 0b00000000),
+            arrayOf(0b00111111, 0b00110000),
+            arrayOf(0b00001111, 0b00111100),
+            arrayOf(0b00000011, 0b00111111)
         )
         private val decodeShift = arrayOf(
-            arrayOf(2, -4, 0, 0),
-            arrayOf(0, 4, -2, 0),
-            arrayOf(0, 0, 6, 0)
+            arrayOf(0, 0),
+            arrayOf(2, 4),
+            arrayOf(4, 2),
+            arrayOf(6, 0)
         )
 
         /**
@@ -30,10 +32,10 @@ class Base64 {
          *  @return input に対応する 6 ビット数値
          *  @exception IllegalArgumentException input が Base64 で未定義の文字の場合
          */
-        fun c2b(input: Char): Byte {
+        fun c2b(input: Char): Int {
             for (i in base64CodeTable) {
                 if (i.charMin <= input && input <= i.charMax) {
-                    return (i.codeMin + (input - i.charMin)).toByte()
+                    return (i.codeMin + (input - i.charMin))
                 }
             }
             throw IllegalArgumentException("illegal character for base64: %c".format(input))
@@ -56,34 +58,6 @@ class Base64 {
         }
 
         /**
-         * 6 ビット数値 4 個を、8 ビット数値 3 個に変換する。
-         *
-         * @param input 6 ビット数値列（4 個）
-         * @return 変換結果の 8 ビット数値列（3 個）
-         */
-        fun pack64(input: Array<Byte>): Array<Byte> {
-            if (input.size != decodeMask[0].size){
-                throw IllegalArgumentException("illegal input length: %d".format(input.size))
-            }
-            val inputInt = input.map { it.toInt() }
-            val output = arrayOf<Byte>(0, 0, 0)
-
-            for (i in 0 until output.size) {
-                var outputInt = 0
-                for (j in 0 until input.size) {
-                    val masked = inputInt[j] and decodeMask[i][j]
-                    val shifted = if (decodeShift[i][j] >= 0)
-                                            masked shl decodeShift[i][j]
-                                    else
-                                            masked ushr -decodeShift[i][j]
-                    outputInt = outputInt or shifted
-                }
-                output[i] = outputInt.toByte()
-            }
-           return output
-        }
-
-        /**
          * 入力を Base64 デコードして得られるバイト列を返す。
          *
          * @param input Base64 文字列
@@ -91,13 +65,17 @@ class Base64 {
          * @return input に対応するバイト列
          */
         fun decode(input: String, flags: Int): ByteArray {
-            val inputCode = arrayOf<Byte>(0, 0, 0, 0)
             val output = mutableListOf<Byte>()
 
             for ((index, c) in input.withIndex()) {
-                inputCode[index % inputCode.size] = c2b(c)
-                if ((index+1) % inputCode.size == 0) {
-                    pack64(inputCode).forEach { output.add(it) }
+                if (index % 4 != 0 && c != '=') {
+                    // 4 文字単位の 1 文字目は出力せず、そのまま（1 文字目 +）2 文字目の処理に移る
+                    // = の場合は出力せずそのままスキップ（末尾の端数処理に使われる）
+                    val b1 = c2b(input[index - 1])
+                    val b2 = c2b(input[index])
+                    val b = ((b1 and decodeMask[index % 4][0]) shl decodeShift[index % 4][0]) or
+                            ((b2 and decodeMask[index % 4][1]) ushr decodeShift[index % 4][1])
+                    output.add(b.toByte())
                 }
             }
             return output.toByteArray()
--- android/app/src/test/java/jp/eagan/tlscertviewer/Base64Test.kt
+++ android/app/src/test/java/jp/eagan/tlscertviewer/Base64Test.kt
@@ -4,70 +4,70 @@ import org.junit.Test
 import org.junit.Assert.assertEquals
 
 class Base64Test {
-    @Test fun c2b_00() { assertEquals(0x00.toByte(), Base64.c2b('A')) }
-    @Test fun c2b_01() { assertEquals(0x01.toByte(), Base64.c2b('B')) }
-    @Test fun c2b_02() { assertEquals(0x02.toByte(), Base64.c2b('C')) }
-    @Test fun c2b_03() { assertEquals(0x03.toByte(), Base64.c2b('D')) }
-    @Test fun c2b_04() { assertEquals(0x04.toByte(), Base64.c2b('E')) }
-    @Test fun c2b_05() { assertEquals(0x05.toByte(), Base64.c2b('F')) }
-    @Test fun c2b_06() { assertEquals(0x06.toByte(), Base64.c2b('G')) }
-    @Test fun c2b_07() { assertEquals(0x07.toByte(), Base64.c2b('H')) }
-    @Test fun c2b_08() { assertEquals(0x08.toByte(), Base64.c2b('I')) }
-    @Test fun c2b_09() { assertEquals(0x09.toByte(), Base64.c2b('J')) }
-    @Test fun c2b_0a() { assertEquals(0x0a.toByte(), Base64.c2b('K')) }
-    @Test fun c2b_0b() { assertEquals(0x0b.toByte(), Base64.c2b('L')) }
-    @Test fun c2b_0c() { assertEquals(0x0c.toByte(), Base64.c2b('M')) }
-    @Test fun c2b_0d() { assertEquals(0x0d.toByte(), Base64.c2b('N')) }
-    @Test fun c2b_0e() { assertEquals(0x0e.toByte(), Base64.c2b('O')) }
-    @Test fun c2b_0f() { assertEquals(0x0f.toByte(), Base64.c2b('P')) }
-    @Test fun c2b_10() { assertEquals(0x10.toByte(), Base64.c2b('Q')) }
-    @Test fun c2b_11() { assertEquals(0x11.toByte(), Base64.c2b('R')) }
-    @Test fun c2b_12() { assertEquals(0x12.toByte(), Base64.c2b('S')) }
-    @Test fun c2b_13() { assertEquals(0x13.toByte(), Base64.c2b('T')) }
-    @Test fun c2b_14() { assertEquals(0x14.toByte(), Base64.c2b('U')) }
-    @Test fun c2b_15() { assertEquals(0x15.toByte(), Base64.c2b('V')) }
-    @Test fun c2b_16() { assertEquals(0x16.toByte(), Base64.c2b('W')) }
-    @Test fun c2b_17() { assertEquals(0x17.toByte(), Base64.c2b('X')) }
-    @Test fun c2b_18() { assertEquals(0x18.toByte(), Base64.c2b('Y')) }
-    @Test fun c2b_19() { assertEquals(0x19.toByte(), Base64.c2b('Z')) }
-    @Test fun c2b_1a() { assertEquals(0x1a.toByte(), Base64.c2b('a')) }
-    @Test fun c2b_1b() { assertEquals(0x1b.toByte(), Base64.c2b('b')) }
-    @Test fun c2b_1c() { assertEquals(0x1c.toByte(), Base64.c2b('c')) }
-    @Test fun c2b_1d() { assertEquals(0x1d.toByte(), Base64.c2b('d')) }
-    @Test fun c2b_1e() { assertEquals(0x1e.toByte(), Base64.c2b('e')) }
-    @Test fun c2b_1f() { assertEquals(0x1f.toByte(), Base64.c2b('f')) }
-    @Test fun c2b_20() { assertEquals(0x20.toByte(), Base64.c2b('g')) }
-    @Test fun c2b_21() { assertEquals(0x21.toByte(), Base64.c2b('h')) }
-    @Test fun c2b_22() { assertEquals(0x22.toByte(), Base64.c2b('i')) }
-    @Test fun c2b_23() { assertEquals(0x23.toByte(), Base64.c2b('j')) }
-    @Test fun c2b_24() { assertEquals(0x24.toByte(), Base64.c2b('k')) }
-    @Test fun c2b_25() { assertEquals(0x25.toByte(), Base64.c2b('l')) }
-    @Test fun c2b_26() { assertEquals(0x26.toByte(), Base64.c2b('m')) }
-    @Test fun c2b_27() { assertEquals(0x27.toByte(), Base64.c2b('n')) }
-    @Test fun c2b_28() { assertEquals(0x28.toByte(), Base64.c2b('o')) }
-    @Test fun c2b_29() { assertEquals(0x29.toByte(), Base64.c2b('p')) }
-    @Test fun c2b_2a() { assertEquals(0x2a.toByte(), Base64.c2b('q')) }
-    @Test fun c2b_2b() { assertEquals(0x2b.toByte(), Base64.c2b('r')) }
-    @Test fun c2b_2c() { assertEquals(0x2c.toByte(), Base64.c2b('s')) }
-    @Test fun c2b_2d() { assertEquals(0x2d.toByte(), Base64.c2b('t')) }
-    @Test fun c2b_2e() { assertEquals(0x2e.toByte(), Base64.c2b('u')) }
-    @Test fun c2b_2f() { assertEquals(0x2f.toByte(), Base64.c2b('v')) }
-    @Test fun c2b_30() { assertEquals(0x30.toByte(), Base64.c2b('w')) }
-    @Test fun c2b_31() { assertEquals(0x31.toByte(), Base64.c2b('x')) }
-    @Test fun c2b_32() { assertEquals(0x32.toByte(), Base64.c2b('y')) }
-    @Test fun c2b_33() { assertEquals(0x33.toByte(), Base64.c2b('z')) }
-    @Test fun c2b_34() { assertEquals(0x34.toByte(), Base64.c2b('0')) }
-    @Test fun c2b_35() { assertEquals(0x35.toByte(), Base64.c2b('1')) }
-    @Test fun c2b_36() { assertEquals(0x36.toByte(), Base64.c2b('2')) }
-    @Test fun c2b_37() { assertEquals(0x37.toByte(), Base64.c2b('3')) }
-    @Test fun c2b_38() { assertEquals(0x38.toByte(), Base64.c2b('4')) }
-    @Test fun c2b_39() { assertEquals(0x39.toByte(), Base64.c2b('5')) }
-    @Test fun c2b_3a() { assertEquals(0x3a.toByte(), Base64.c2b('6')) }
-    @Test fun c2b_3b() { assertEquals(0x3b.toByte(), Base64.c2b('7')) }
-    @Test fun c2b_3c() { assertEquals(0x3c.toByte(), Base64.c2b('8')) }
-    @Test fun c2b_3d() { assertEquals(0x3d.toByte(), Base64.c2b('9')) }
-    @Test fun c2b_3e() { assertEquals(0x3e.toByte(), Base64.c2b('+')) }
-    @Test fun c2b_3f() { assertEquals(0x3f.toByte(), Base64.c2b('/')) }
+    @Test fun c2b_00() { assertEquals(0x00, Base64.c2b('A')) }
+    @Test fun c2b_01() { assertEquals(0x01, Base64.c2b('B')) }
+    @Test fun c2b_02() { assertEquals(0x02, Base64.c2b('C')) }
+    @Test fun c2b_03() { assertEquals(0x03, Base64.c2b('D')) }
+    @Test fun c2b_04() { assertEquals(0x04, Base64.c2b('E')) }
+    @Test fun c2b_05() { assertEquals(0x05, Base64.c2b('F')) }
+    @Test fun c2b_06() { assertEquals(0x06, Base64.c2b('G')) }
+    @Test fun c2b_07() { assertEquals(0x07, Base64.c2b('H')) }
+    @Test fun c2b_08() { assertEquals(0x08, Base64.c2b('I')) }
+    @Test fun c2b_09() { assertEquals(0x09, Base64.c2b('J')) }
+    @Test fun c2b_0a() { assertEquals(0x0a, Base64.c2b('K')) }
+    @Test fun c2b_0b() { assertEquals(0x0b, Base64.c2b('L')) }
+    @Test fun c2b_0c() { assertEquals(0x0c, Base64.c2b('M')) }
+    @Test fun c2b_0d() { assertEquals(0x0d, Base64.c2b('N')) }
+    @Test fun c2b_0e() { assertEquals(0x0e, Base64.c2b('O')) }
+    @Test fun c2b_0f() { assertEquals(0x0f, Base64.c2b('P')) }
+    @Test fun c2b_10() { assertEquals(0x10, Base64.c2b('Q')) }
+    @Test fun c2b_11() { assertEquals(0x11, Base64.c2b('R')) }
+    @Test fun c2b_12() { assertEquals(0x12, Base64.c2b('S')) }
+    @Test fun c2b_13() { assertEquals(0x13, Base64.c2b('T')) }
+    @Test fun c2b_14() { assertEquals(0x14, Base64.c2b('U')) }
+    @Test fun c2b_15() { assertEquals(0x15, Base64.c2b('V')) }
+    @Test fun c2b_16() { assertEquals(0x16, Base64.c2b('W')) }
+    @Test fun c2b_17() { assertEquals(0x17, Base64.c2b('X')) }
+    @Test fun c2b_18() { assertEquals(0x18, Base64.c2b('Y')) }
+    @Test fun c2b_19() { assertEquals(0x19, Base64.c2b('Z')) }
+    @Test fun c2b_1a() { assertEquals(0x1a, Base64.c2b('a')) }
+    @Test fun c2b_1b() { assertEquals(0x1b, Base64.c2b('b')) }
+    @Test fun c2b_1c() { assertEquals(0x1c, Base64.c2b('c')) }
+    @Test fun c2b_1d() { assertEquals(0x1d, Base64.c2b('d')) }
+    @Test fun c2b_1e() { assertEquals(0x1e, Base64.c2b('e')) }
+    @Test fun c2b_1f() { assertEquals(0x1f, Base64.c2b('f')) }
+    @Test fun c2b_20() { assertEquals(0x20, Base64.c2b('g')) }
+    @Test fun c2b_21() { assertEquals(0x21, Base64.c2b('h')) }
+    @Test fun c2b_22() { assertEquals(0x22, Base64.c2b('i')) }
+    @Test fun c2b_23() { assertEquals(0x23, Base64.c2b('j')) }
+    @Test fun c2b_24() { assertEquals(0x24, Base64.c2b('k')) }
+    @Test fun c2b_25() { assertEquals(0x25, Base64.c2b('l')) }
+    @Test fun c2b_26() { assertEquals(0x26, Base64.c2b('m')) }
+    @Test fun c2b_27() { assertEquals(0x27, Base64.c2b('n')) }
+    @Test fun c2b_28() { assertEquals(0x28, Base64.c2b('o')) }
+    @Test fun c2b_29() { assertEquals(0x29, Base64.c2b('p')) }
+    @Test fun c2b_2a() { assertEquals(0x2a, Base64.c2b('q')) }
+    @Test fun c2b_2b() { assertEquals(0x2b, Base64.c2b('r')) }
+    @Test fun c2b_2c() { assertEquals(0x2c, Base64.c2b('s')) }
+    @Test fun c2b_2d() { assertEquals(0x2d, Base64.c2b('t')) }
+    @Test fun c2b_2e() { assertEquals(0x2e, Base64.c2b('u')) }
+    @Test fun c2b_2f() { assertEquals(0x2f, Base64.c2b('v')) }
+    @Test fun c2b_30() { assertEquals(0x30, Base64.c2b('w')) }
+    @Test fun c2b_31() { assertEquals(0x31, Base64.c2b('x')) }
+    @Test fun c2b_32() { assertEquals(0x32, Base64.c2b('y')) }
+    @Test fun c2b_33() { assertEquals(0x33, Base64.c2b('z')) }
+    @Test fun c2b_34() { assertEquals(0x34, Base64.c2b('0')) }
+    @Test fun c2b_35() { assertEquals(0x35, Base64.c2b('1')) }
+    @Test fun c2b_36() { assertEquals(0x36, Base64.c2b('2')) }
+    @Test fun c2b_37() { assertEquals(0x37, Base64.c2b('3')) }
+    @Test fun c2b_38() { assertEquals(0x38, Base64.c2b('4')) }
+    @Test fun c2b_39() { assertEquals(0x39, Base64.c2b('5')) }
+    @Test fun c2b_3a() { assertEquals(0x3a, Base64.c2b('6')) }
+    @Test fun c2b_3b() { assertEquals(0x3b, Base64.c2b('7')) }
+    @Test fun c2b_3c() { assertEquals(0x3c, Base64.c2b('8')) }
+    @Test fun c2b_3d() { assertEquals(0x3d, Base64.c2b('9')) }
+    @Test fun c2b_3e() { assertEquals(0x3e, Base64.c2b('+')) }
+    @Test fun c2b_3f() { assertEquals(0x3f, Base64.c2b('/')) }
 
     @Test(expected = IllegalArgumentException::class)
     fun c2b_Exception() {
@@ -145,65 +145,6 @@ class Base64Test {
     }
 
     @Test
-    fun pack64_Zeros() {
-        val packed = Base64.pack64(arrayOf(0, 0, 0, 0))
-        assertEquals(3, packed.size)
-        assertEquals(0x00.toByte(), packed[0])
-        assertEquals(0x00.toByte(), packed[1])
-        assertEquals(0x00.toByte(), packed[2])
-    }
-
-    @Test
-    fun pack64_single_01() {
-        val packed = Base64.pack64(arrayOf(0x3f, 0, 0, 0))
-        assertEquals(3, packed.size)
-        assertEquals(0b1111_1100.toByte(), packed[0])
-        assertEquals(0b0000_0000.toByte(), packed[1])
-        assertEquals(0b0000_0000.toByte(), packed[2])
-    }
-
-    @Test
-    fun pack64_single_02() {
-        val packed = Base64.pack64(arrayOf(0, 0x3f, 0, 0))
-        assertEquals(3, packed.size)
-        assertEquals(0b0000_0011.toByte(), packed[0])
-        assertEquals(0b1111_0000.toByte(), packed[1])
-        assertEquals(0b0000_0000.toByte(), packed[2])
-    }
-
-    @Test
-    fun pack64_single_03() {
-        val packed = Base64.pack64(arrayOf(0, 0, 0x3f, 0))
-        assertEquals(3, packed.size)
-        assertEquals(0b0000_0000.toByte(), packed[0])
-        assertEquals(0b0000_1111.toByte(), packed[1])
-        assertEquals(0b1100_0000.toByte(), packed[2])
-    }
-
-    @Test
-    fun pack64_single_04() {
-        val packed = Base64.pack64(arrayOf(0, 0, 0, 0x3f))
-        assertEquals(3, packed.size)
-        assertEquals(0b0000_0000.toByte(), packed[0])
-        assertEquals(0b0000_0000.toByte(), packed[1])
-        assertEquals(0b0011_1111.toByte(), packed[2])
-    }
-
-    @Test
-    fun decode_Zeros() {
-        val decoded = Base64.decode("AAAAAAAAAAAA", 0)
-        assertEquals(9, decoded.size)
-        assertEquals(0x00.toByte(), decoded[0])
-        assertEquals(0x00.toByte(), decoded[1])
-        assertEquals(0x00.toByte(), decoded[2])
-        assertEquals(0x00.toByte(), decoded[3])
-        assertEquals(0x00.toByte(), decoded[4])
-        assertEquals(0x00.toByte(), decoded[5])
-        assertEquals(0x00.toByte(), decoded[6])
-        assertEquals(0x00.toByte(), decoded[7])
-        assertEquals(0x00.toByte(), decoded[8])
-    }
-    @Test
     fun decode_01() {
         val decoded = Base64.decode("ghijklmnopqr", 0)
         assertEquals(9, decoded.size)
@@ -216,5 +157,32 @@ class Base64Test {
         assertEquals(0b101000_10.toByte(), decoded[6]) // op
         assertEquals(0b1001_1010.toByte(), decoded[7]) // pq
         assertEquals(0b10_101011.toByte(), decoded[8]) // qr
+    }
+
+    @Test
+    fun decode_odd01() {
+        val decoded = Base64.decode("ghijklmnopq=", 0)
+        assertEquals(8, decoded.size)
+        assertEquals(0b100000_10.toByte(), decoded[0]) // gh
+        assertEquals(0b0001_1000.toByte(), decoded[1]) // hi
+        assertEquals(0b10_100011.toByte(), decoded[2]) // ij
+        assertEquals(0b100100_10.toByte(), decoded[3]) // kl
+        assertEquals(0b0101_1001.toByte(), decoded[4]) // lm
+        assertEquals(0b10_100111.toByte(), decoded[5]) // mn
+        assertEquals(0b101000_10.toByte(), decoded[6]) // op
+        assertEquals(0b1001_1010.toByte(), decoded[7]) // pq
+    }
+
+    @Test
+    fun decode_odd02() {
+        val decoded = Base64.decode("ghijklmnop==", 0)
+        assertEquals(7, decoded.size)
+        assertEquals(0b100000_10.toByte(), decoded[0]) // gh
+        assertEquals(0b0001_1000.toByte(), decoded[1]) // hi
+        assertEquals(0b10_100011.toByte(), decoded[2]) // ij
+        assertEquals(0b100100_10.toByte(), decoded[3]) // kl
+        assertEquals(0b0101_1001.toByte(), decoded[4]) // lm
+        assertEquals(0b10_100111.toByte(), decoded[5]) // mn
+        assertEquals(0b101000_10.toByte(), decoded[6]) // op
     }
 }
